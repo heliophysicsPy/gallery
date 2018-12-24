1e13184b689b4835d8fcaa85d67c3777b173908e
Steven Christe <steven.d.christe@nasa.gov>
2018-12-24 14:32:32 +0100

    update to units_demo to use intersphinx

--- gallery/units_demo.py
+++ gallery/units_demo.py
@@ -5,45 +5,43 @@ Quantities, Units, and Constants
 ================================
 
 The purpose of this demo is to demonstrate the capabilities of astropy
-units, quantities, and constants.
+`~astropy.units.Unit`, `~astropy.units.Quantity`, and `~astropy.units.Constant`.
 """
 
 ##############################################################################
-# `astropy.units <http://docs.astropy.org/en/stable/units/index.html>`_
+# The astropy `~astropy.units.Quantity` object
 # handles defining, converting between, and performing arithmetic
 # with physical quantities, such as meters, seconds, Hz, etc.
 from astropy import units as u
 import numpy as np
 
 ##############################################################################
-# You can define a `quantity <http://docs.astropy.org/en/stable/units/>`_
+# You can define a `~astropy.units.Quantity`
 # (a number with a unit) in a number of different ways.
 42.0 * u.meter
 [1., 2., 3.] * u.s
 np.arange(10) * u.Hz
 
 ##############################################################################
-# Units work as you would expect with most python operators or numpy functions
+# These objects work as you would expect with most Python operators or numpy
+# functions
 np.power(2 * u.s, 3)
 (2 * u.s) ** 2
 
 ##############################################################################
-# and of course the following provides the appropriate error
-np.exp(4 * u.s)
-
-##############################################################################
-# if needed you can get the value as well as the unit
+# If needed you can get the value as well as the unit
 q = 42.0 * u.meter
 print("The value is {0} and the unit is {1}".format(q.value, q.unit))
 
 ##############################################################################
-# Units can easily be converted using the following operator
-q.to('parsec')
+# Using the `~astropy.units.Quantity.to` function we can easily converted to
+# another unit.
+print(q.to('parsec'))
 
 ##############################################################################
 # and imperial units as also supported
 from astropy.units import imperial
-q.to(imperial.mile)
+print(q.to(imperial.mile))
 
 ##############################################################################
 # Units that “cancel out” become a special unit called the “dimensionless unit”:
@@ -51,12 +49,12 @@ u.m / u.m
 
 ##############################################################################
 # More complex conversions are also supported using
-# `equivalency<http://docs.astropy.org/en/stable/units/equivalencies.html>`_`
-# For example, we can convert the GOES wavelength range to Hz or keV easily using the
-# appropriate spectral equivalency.
-([0.5, 4.0] * u.angstrom).to('Hz', u.spectral())
+# `~astropy.units.equivalencies`.
+# For example, we can convert the GOES wavelength range to Hz or keV easily using
+# the `~astropy.units.equivalencies.spectral`.
+print(([0.5, 4.0] * u.angstrom).to('Hz', u.spectral()))
 
-([0.5, 4.0] * u.angstrom).to('keV', u.spectral())
+print(([0.5, 4.0] * u.angstrom).to('keV', u.spectral()))
 
 ##############################################################################
 # Astropy provides a number of reference constants
@@ -67,14 +65,14 @@ from astropy import constants as astropy_const
 from sunpy.sun import constants as sunpy_const
 
 ##############################################################################
-# Constants are simply quantities but they also provide an uncertainty
+# `~astropy.units.Constant` are simply quantities but they also provide an uncertainty
 # and a reference
 M_earth = astropy_const.M_earth
 print("The mass of the Earth is {0} +/- {1} {2} [ref {3}].".format(M_earth.value, M_earth.uncertainty, M_earth.unit, M_earth.reference))
 
 ##############################################################################
 # The light travel time in minutes from the Sun to the Earth can be calculated
-(sunpy_const.au / astropy_const.c).to('min')
+print((sunpy_const.au / astropy_const.c).to('min'))
 
 ##############################################################################
 # Let's define a function to calculate the plasma beta, with quantities we
@@ -85,7 +83,7 @@ def plasma_beta(n, T, B):
 ##############################################################################
 # The plasma beta for the solar corona using appropriate parameters is given
 # by the following. The decompose function works to simplify the units.
-plasma_beta(1e9 * u.cm**-3, 3e6 * u.Kelvin, 10 * u.Gauss).decompose()
+print(plasma_beta(1e9 * u.cm**-3, 3e6 * u.Kelvin, 10 * u.Gauss).decompose())
 
 ##############################################################################
 # If the input is given in the wrong units then an error may occur but a better
