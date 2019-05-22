038572e06f26cdc19f91eecee8adce231b143690
Bryan Harter <41062454+bryan-harter@users.noreply.github.com>
2019-05-22 16:45:55 -0600

    Update conf.py

--- conf.py
+++ conf.py
@@ -94,7 +94,7 @@ html_theme_options = {}
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-# html_static_path = ['_static']
+html_static_path = ['_static']
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
@@ -225,3 +225,7 @@ sphinx_gallery_conf = {
     'plot_gallery': True,
     # 'line_numbers': True
 }
+
+def setup(app):
+	app.add_stylesheet('css/custom.css')
+	
