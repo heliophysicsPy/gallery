8e935d255bc12598e56e12865fa814ae9b60cfaa
Shawn Polson <sapols@yahoo.com>
2021-10-27 10:58:37 -0600

    Update pySPEDAS example

--- gallery/pyspedas_demo.py
+++ gallery/pyspedas_demo.py
@@ -10,111 +10,101 @@ The purpose of this demo is to demonstrate some of the capabilities of the pyspe
 """
 
 ####################################################################################
-# The pyspedas package contains functions for downloading data from NASA missions 
-# and tools for data analysis and data plotting. It works together with 
-# the pytplot and the cdflib packages.
+# The pySPEDAS package contains functions for downloading data from NASA missions 
+# and tools for data analysis and data plotting. It works together with the 
+# pytplot and the cdflib packages.
 #
-# Pyspedas is a python implementation of the SPEDAS software which is written 
+# pySPEDAS is a python implementation of the SPEDAS software which is written 
 # in the IDL programming language. For more information on SPEDAS, 
 # see: http://spedas.org/wiki/
-from pyspedas import load_data, gmag_list, subtract_average, version
-from pytplot import del_data, get_data, store_data, ylim, tplot, tplot_options, tplot_names
+import pyspedas
+import pytplot
+from pyspedas import clean_spikes, version
+from pytplot import del_data, get_data, store_data, tplot, tplot_options, tplot_names
 
 ####################################################################################
 # Load and plot THEMIS data
 ####################################################################################
 
 ####################################################################################
-# Pyspedas uses a preferences file (spd_prefs_txt.py) which contains the variables
-# that determine the local path for saving cdf files. The user can change this
-# path, if he wants to. We can find the location of the pyspedas preferences file
-# and the version of the installed pyspedas package, using the function version(). 
+# Note: pySPEDAS uses an environment variable SPEDAS_DATA_DIR to determine the 
+# local path for saving data files. SPEDAS_DATA_DIR acts as a root data directory 
+# for all missions, but mission-specific data directories 
+# (e.g., MMS_DATA_DIR for MMS, THM_DATA_DIR for THEMIS) can also be set, 
+# and these will override SPEDAS_DATA_DIR.
+#
+# We can find the version of the installed pyspedas package using `version()`.  
 version()
 
 ####################################################################################
-# Delete any existing pytplot variables
+# Delete any existing pytplot variables.
 del_data()
 
 ####################################################################################
-# Define a time range. Here, we pick a time range that spans one and a half day.
-time_range = ['2015-12-31 00:00:00', '2016-01-01 12:00:00']
+# Define a time range. Here, we pick a time range that spans one day.
+time_range = ['2015-10-16', '2015-10-17']
 
 ####################################################################################
-# Download THEMIS state data and store it into the pytplot object.
-# This following function downloads all the necessary files, loads data, 
-# and time-clips data to the specified time range. 
-load_data('themis', time_range, ['tha'], 'state', 'l1')
+# You can load data into tplot variables by calling `pyspedas.mission.instrument()`. 
+# E.g., to load and plot our one day of THEMIS FGM data for probe 'd':
+#
+# (This following function downloads all the necessary files, loads data, 
+# and time-clips data to the specified time range.)
+thm_fgm = pyspedas.themis.fgm(trange=time_range, probe='d')
 
 ####################################################################################
+# Mission-specific information and examples can be found in the READMEs of each 
+# mission directory in the pySPEDAS GitHub repo. E.g., THEMIS: 
+# https://github.com/spedas/pyspedas/tree/master/pyspedas/themis
+#
 # Get data from pytplot object into python variables. 
 # This is useful when we want to work on the data using standard python libraries.
-alldata = get_data("tha_vel")
-time = alldata[0]
-data = alldata[1]
+all_data = get_data("thd_fgs_gse")
+time = all_data[0]
+data = all_data[1]
 
 ####################################################################################
 # After working with the data, we can store a new pytplot variable. 
 # We can store any data in the pytplot object. 
-store_data("tha_new_vel", data={'x': time, 'y': data})
+store_data("new_thd_fgs_gse", data={'x': time, 'y': data})
 
 ####################################################################################
-# Preparing for the plots, we define the y-axis limits.
-ylim('tha_pos', -23000.0, 81000.0)
-ylim('tha_new_vel', -8.0, 12.0)
-
-####################################################################################
-# We plot the position and the velocity using the pyqtgraph library (the default). 
+# We plot the data using the pyqtgraph library (the default). 
 # Another option is to plot using the bokeh library.
-tplot(["tha_pos", "tha_new_vel"])
-
+tplot(['thd_fgs_gse', 'thd_fgs_gsm'])
 
 ####################################################################################
-# Load and plot GMAG data
+# Load and plot MMS data
 ####################################################################################
 
 ####################################################################################
 # Delete any existing pytplot variables, and define a time range.  
 del_data()
-time_range = ['2015-12-31 00:00:00', '2015-12-31 23:59:59']
+time_range = ['2015-10-16/13:05:30', '2015-10-16/13:07:30']
 
 ####################################################################################
-# GMAG stations are organized in groups. For a list of all available GMAG groups 
-# and all GMAG stations, see: http://themis.ssl.berkeley.edu/gmag/gmag_list.php
+# Load and plot two minutes of MMS burst mode FGM data:
 #
-# Get a list of the GMAG stations that belong to the EPO group.
-sites = gmag_list(group='epo')
+# (Note that it prompts you for an SDC username before the download. 
+# You can submit a blank username.)
+mms_fgm = pyspedas.mms.fgm(trange=['2015-10-16/13:05:30', '2015-10-16/13:07:30'], data_rate='brst')
 
 ####################################################################################
-# Download cdf files for all EPO GMAG stations and load data into pytplot variables.
-#
-# Some GMAG stations may not have any data files for the specified time interval. 
-# In that case, we will get an error message that the remote file does not exist
-# for that GMAG station.  
-load_data('gmag', time_range, sites, '', '')
+# The names of the loaded tplot variables are printed. You can print the names of 
+# all currently-loaded tplot variables at any time using `pyspedas.tnames()`:
+pyspedas.tnames()
 
 ####################################################################################
-# Print the names of the loaded GMAG sites. 
-sites_loaded = tplot_names()
+# pySPEDAS has a number of helpful analysis routines under `pyspedas.analysis`. 
+# E.g., if we want to clean spikes from the data:
+clean_spikes(['mms1_fgm_b_gse_brst_l2', 'mms1_fgm_b_gsm_brst_l2'])
 
 ####################################################################################
-# Subtract the average values for these sites.
-subtract_average(sites_loaded, '')
+# Plot the (despiked) MMS data. 
+# Use the bokeh library — the plots will appear in the web browser.
+tplot(['mms1_fgm_b_gse_brst_l2-despike', 'mms1_fgm_b_gsm_brst_l2-despike'], bokeh=True)
 
 ####################################################################################
-# Download AE index data.
-# load_data('gmag', time_range, ['idx'], '', '')
-
-# ####################################################################################
-# # Get a list of all the loaded GMAG sites plus the AE index data. 
-# sites_loaded = tplot_names()
-
-# ####################################################################################
-# # Plot GMAG and AE index data.
-# # Use the bokeh library - the plots will appear in the web browser.
-# tplot_options('title', 'EPO GMAG 2015-12-31')
-# tplot(sites_loaded, bokeh=True)
-
-# ####################################################################################
-# # Note: The HTML web page for this example may be missing the plots but this is a 
-# # limitation of the platform for this particular gallery - 
-# # if you run the python code locally, the plots will appear. 
+# Note: The HTML web page for this example may be missing the plots but this is a 
+# limitation of the platform for this particular gallery — if you run the 
+# python code locally, the plots will appear. 
