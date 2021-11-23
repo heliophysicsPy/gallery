c1be7aaaaf80213ed9b1b810ead014eadd109a1e
sapols <sapols@yahoo.com>
2021-11-22 22:53:11 -0700

    Only uncomment all pyspedas code

--- gallery/pyspedas_demo.py
+++ gallery/pyspedas_demo.py
@@ -17,10 +17,10 @@ The purpose of this demo is to demonstrate some of the capabilities of the pyspe
 # pySPEDAS is a python implementation of the SPEDAS software which is written 
 # in the IDL programming language. For more information on SPEDAS, 
 # see: http://spedas.org/wiki/
-# import pyspedas
-# import pytplot
-# from pyspedas import clean_spikes, version
-# from pytplot import del_data, get_data, store_data, tplot_options, tplot_names, tplot
+import pyspedas
+import pytplot
+from pyspedas import clean_spikes, version
+from pytplot import del_data, get_data, store_data, tplot_options, tplot_names, tplot
 
 ####################################################################################
 # Load and plot THEMIS data
@@ -34,15 +34,15 @@ The purpose of this demo is to demonstrate some of the capabilities of the pyspe
 # and these will override SPEDAS_DATA_DIR.
 #
 # We can find the version of the installed pyspedas package using `version()`.  
-# version()
+version()
 
 ####################################################################################
 # Delete any existing pytplot variables.
-# del_data()
+del_data()
 
 ####################################################################################
 # Define a time range. Here, we pick a time range that spans one day.
-# time_range = ['2015-10-16', '2015-10-17']
+time_range = ['2015-10-16', '2015-10-17']
 
 ####################################################################################
 # You can load data into tplot variables by calling `pyspedas.mission.instrument()`. 
@@ -50,7 +50,7 @@ The purpose of this demo is to demonstrate some of the capabilities of the pyspe
 #
 # (This following function downloads all the necessary files, loads data, 
 # and time-clips data to the specified time range.)
-# thm_fgm = pyspedas.themis.fgm(trange=time_range, probe='d')
+thm_fgm = pyspedas.themis.fgm(trange=time_range, probe='d')
 
 ####################################################################################
 # Mission-specific information and examples can be found in the READMEs of each 
@@ -59,19 +59,19 @@ The purpose of this demo is to demonstrate some of the capabilities of the pyspe
 #
 # Get data from pytplot object into python variables. 
 # This is useful when we want to work on the data using standard python libraries.
-# all_data = get_data("thd_fgs_gse")
-# time = all_data[0]
-# data = all_data[1]
+all_data = get_data("thd_fgs_gse")
+time = all_data[0]
+data = all_data[1]
 
 ####################################################################################
 # After working with the data, we can store a new pytplot variable. 
 # We can store any data in the pytplot object. 
-# store_data("new_thd_fgs_gse", data={'x': time, 'y': data})
+store_data("new_thd_fgs_gse", data={'x': time, 'y': data})
 
 ####################################################################################
 # We plot the data using the pyqtgraph library (the default). 
 # Another option is to plot using the bokeh library.
-# tplot(['thd_fgs_gse', 'thd_fgs_gsm'])
+tplot(['thd_fgs_gse', 'thd_fgs_gsm'])
 
 ####################################################################################
 # Load and plot MMS data
@@ -79,25 +79,25 @@ The purpose of this demo is to demonstrate some of the capabilities of the pyspe
 
 ####################################################################################
 # Delete any existing pytplot variables, and define a time range.  
-# del_data()
-# time_range = ['2015-10-16/13:05:30', '2015-10-16/13:07:30']
+del_data()
+time_range = ['2015-10-16/13:05:30', '2015-10-16/13:07:30']
 
 ####################################################################################
 # Load and plot two minutes of MMS burst mode FGM data:
 #
 # (Note that it prompts you for an SDC username before the download. 
 # You can submit a blank username.)
-# mms_fgm = pyspedas.mms.fgm(trange=['2015-10-16/13:05:30', '2015-10-16/13:07:30'], data_rate='brst')
+mms_fgm = pyspedas.mms.fgm(trange=['2015-10-16/13:05:30', '2015-10-16/13:07:30'], data_rate='brst')
 
 ####################################################################################
 # The names of the loaded tplot variables are printed. You can print the names of 
 # all currently-loaded tplot variables at any time using `pyspedas.tnames()`:
-# pyspedas.tnames()
+pyspedas.tnames()
 
 ####################################################################################
 # pySPEDAS has a number of helpful analysis routines under `pyspedas.analysis`. 
 # E.g., if we want to clean spikes from the data:
-# clean_spikes(['mms1_fgm_b_gse_brst_l2', 'mms1_fgm_b_gsm_brst_l2'])
+clean_spikes(['mms1_fgm_b_gse_brst_l2', 'mms1_fgm_b_gsm_brst_l2'])
 
 ####################################################################################
 # Plot the (despiked) MMS data. 
@@ -105,9 +105,9 @@ The purpose of this demo is to demonstrate some of the capabilities of the pyspe
 #
 # Note how we use pytplot options to set the line colors. See the full list of 
 # options at: https://pytplot.readthedocs.io/en/latest/_modules/pytplot/options.html
-# pytplot.options('mms1_fgm_b_gse_brst_l2-despike', 'color', 'red')
-# pytplot.options('mms1_fgm_b_gsm_brst_l2-despike', 'color', 'blue')
-# tplot(['mms1_fgm_b_gse_brst_l2-despike', 'mms1_fgm_b_gsm_brst_l2-despike'], bokeh=True)
+pytplot.options('mms1_fgm_b_gse_brst_l2-despike', 'color', 'red')
+pytplot.options('mms1_fgm_b_gsm_brst_l2-despike', 'color', 'blue')
+tplot(['mms1_fgm_b_gse_brst_l2-despike', 'mms1_fgm_b_gsm_brst_l2-despike'], bokeh=True)
 
 ####################################################################################
 # Note: The HTML web page for this example may be missing the plots but this is a 
