7e2e4109c85e451910afa8cb3d92a9dd292ee691
Nick Murphy <namurphy@cfa.harvard.edu>
2022-07-06 11:23:37 -0400

    Change language configuration

--- conf.py
+++ conf.py
@@ -23,7 +23,6 @@ import pathlib
 project = 'PyHC Tutorials'
 copyright = '2018–2022, PyHC'
 author = 'PyHC'
-language = "en"
 
 # The short X.Y version
 version = ''
@@ -61,7 +60,7 @@ master_doc = 'index'
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = "en"
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
